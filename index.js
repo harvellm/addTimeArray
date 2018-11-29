e918fbd7e7a4b84a26bc7be734a07a103cd23edc
MatthewHarvell <matthewharvell@pop-os.localdomain>
2018-11-28 20:04:57 -0600

    working on all tests with named regex

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
 
 const moment = require('moment');
-var addTimeArray = (timeArray) => {
-    const splitter = /^(\d{0,2}:)?(\d{0,2}:)(\d{0,2})$|^(\d{0,2})$/;
+const namedRegexp = require("named-js-regexp");
+
+const formatTime = timePart => {
+    if(!Number.isInteger(timePart)){
+        throw new TypeError('Parameter must be an integer.') //should never happen unless Moment coughs
+    }
+    return timePart.toString(10).padStart(2,'0');
+}
+const addTimeArray = (timeArray) => {
+    const splitter = /^(?:(:<days>\d+):)?(:<hours>\d{0,2}:)(:<minutes>[0-5]?[0-9]:)(:<seconds>[0-5]?[0-9])$|^(:<hours>\d{0,2}:)?(:<minutes>[0-5]?[0-9]:)(:<seconds>[0-5]?[0-9])$|^(:<seconds>[0-5]?[0-9])$/;
+    let re = namedRegexp(splitter);
     let totalTime = moment.duration("00:00:00");
     let badValues = [];
     let returnValue;
     for (time of timeArray) {
         try{
-            let [,hours,minutes,seconds,secondsOnly] = splitter.exec(time);
+            let {days,hours,minutes,seconds} = re.execGroups(time,false);
+            days && (days = `${days}.`);
             !hours && (hours = '00:');
             !minutes && (minutes = '00:');
-            secondsOnly && (seconds = secondsOnly);
-            totalTime.add(moment.duration(`${hours}${minutes}${seconds}`));
+            totalTime.add(moment.duration(`${(days)?days:''}${hours}${minutes}${seconds}`));
         } catch(e){
             returnValue = {
                 status:'error'
@@ -22,7 +31,7 @@ var addTimeArray = (timeArray) => {
     if(!returnValue){
         return {
             status:'success',
-            text:`${totalTime.hours().toString(10).padStart(2,'0')}:${totalTime.minutes().toString(10).padStart(2,'0')}:${totalTime.seconds().toString(10).padStart(2,'0')}`
+            text:`${formatTime(totalTime.days())}:${formatTime(totalTime.hours())}:${formatTime(totalTime.minutes())}:${formatTime(totalTime.seconds())}`
         };
     }  else {
         returnValue.text=badValues.join(',');
@@ -31,6 +40,6 @@ var addTimeArray = (timeArray) => {
      
 }
 module.exports = addTimeArray;
-let test1Array = ['59','01'];
-let test1result = addTimeArray(test1Array);
- console.log(test1result.text);
+let test6Array = ['01:02:00:01','1:00:01'];
+let test1result = addTimeArray(test6Array);
+ //console.log(test1result.text);
--- test/test.js
+++ test/test.js
@@ -1,19 +1,19 @@
 
-var expect = require('chai').expect;
-var sut = require('../index');
+let expect = require('chai').expect;
+let sut = require('../index');
 
 describe('addTimeArray()', function () {
     it('should add two times', function () {
       let test1Array = ['01:00','01:00'];
-      var test1Result= sut(test1Array);
-      expect(test1Result.text).to.be.equal('00:02:00');
+      let test1Result= sut(test1Array);
+      expect(test1Result.text).to.be.equal('00:00:02:00');
       expect(test1Result.status).to.be.equal('success');
     });
 
     it('should add times with hours and without hours', function () {
       let test2Array = ['11:00:01','01:01'];
-      var test2Result = sut(test2Array);
-      expect(test2Result.text).to.be.equal('11:01:02');
+      let test2Result = sut(test2Array);
+      expect(test2Result.text).to.be.equal('00:11:01:02');
       expect(test2Result.status).to.be.equal('success');
     });
 
@@ -22,26 +22,37 @@ describe('addTimeArray()', function () {
       "13:22", "4:41", "8:36", "21:58", "3:06", "10:46", "10:13", "12:54",
       "14:00", "11:03", "16:03", "10:52", "24:53", "10:03", "11:49",
       "15:47", "3:19", "2:06", "5:47", "1:03", "5:29", "5:47", "26:39"];
-      var test3Result = sut(test3Array);
-      expect(test3Result.text).to.be.equal('04:59:03');
+      let test3Result = sut(test3Array);
+      expect(test3Result.text).to.be.equal('00:04:59:03');
       expect(test3Result.status).to.be.equal('success');
     });
 
     it('should return error with bad value if string is not valid time', function () {
       let test4Array = ['11:00:01','French Canadian Elephants'];
-      var test4Result = sut(test4Array);
+      let test4Result = sut(test4Array);
       expect(test4Result.status).to.be.equal('error');
       expect(test4Result.text).that.be.equal('French Canadian Elephants');
     });
     
     it('should treat strings without colons and numbers < 59 as seconds', function() {
 
-        let test5array = ['59','01'];
-        var test5Result = sut(test5array);
-        expect(test5Result.text).to.be.equal('00:01:00');
+        let test5Array = ['59','01'];
+        let test5Result = sut(test5Array);
+        expect(test5Result.text).to.be.equal('00:00:01:00');
         expect(test5Result.status).to.be.equal('success');
+    });
+    it('should handle hours above 24', function() {
+      let test6Array = ['25:00:01','1:00:01'];
+      let test6Result = sut(test6Array);
+      expect(test6Result.text).to.be.equal('01:02:00:02');
+      expect(test6Result.status).to.be.equal('success');
+    });
+    it('should handle adding days', function () {
+      let test6Array = ['01:02:00:01','1:00:01'];
+      let test6Result = sut(test6Array);
+      expect(test6Result.text).to.be.equal('01:03:00:02');
+      expect(test6Result.status).to.be.equal('success');
     });
   });
 
 
-// console.log(`${totalTime.hours()}:${totalTime.minutes()}:${totalTime.seconds()}`);
